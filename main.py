5d2fcdda2f24636c4bc94b15322cfd81b0ef8641
Saurav <Saurav@Ritikas-MacBook-Air.local>
2025-11-24 22:50:11 +0530

    Feature: Added user input and error handling

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ out_of_guesses = False
 
 while guess != secret_number and not(out_of_guesses):
     if guess_count < guess_limit:
+        try:
+            guess = int(input("Guess the number between 1 to 10: "))
+        except ValueError:
+            print("Please enter a number.")
+            continue
         guess_count += 1
     else:
         out_of_guesses = True
+        
