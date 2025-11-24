f9c94286962f7a15f4f27b44c7fff5be6ad97f06
Saurav <Saurav@Ritikas-MacBook-Air.local>
2025-11-24 22:47:48 +0530

    Logic: Added main game loop and limit check

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ guess = None
 guess_count = 0
 guess_limit = 3
 out_of_guesses = False
+
+while guess != secret_number and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess_count += 1
+    else:
+        out_of_guesses = True
